d53403b0f0c601569bd5ebebd6d2b91dc927a260
Yunzhi Liu <yzliu@CNyzliu.local>
2018-07-18 14:36:18 +0800

    practice testing

--- spec/practice-1/main-spec.js
+++ spec/practice-1/main-spec.js
@@ -3,14 +3,19 @@ const Student = require('../../main/practice-1/student');
 const Worker = require('../../main/practice-1/worker');
 
 describe("Person", () => {
+  let person;
+  beforeAll((() => {
+    person = new Person("Tom", 21);
+    console.log("all");
+  }));
   it("should have field name and age", () => {
-    let person = new Person("Tom", 21);
+    // let person = new Person("Tom", 21);
     expect(person.name).toBe("Tom");
     expect(person.age).toBe(21);
   });
 
   it("should have a method introduce, introduce person with name and age", () => {
-    let person = new Person("Tom", 21);
+    // let person = new Person("Tom", 21);
 
     let introduce = person.introduce();
 
@@ -19,8 +24,13 @@ describe("Person", () => {
   });
 
   describe("Student", () => {
+    let student;
+    beforeEach(() => {
+      student = new Student("Tom", 21, 2);
+      console.log("each");
+    });
     it("should have field name, age and class number", () => {
-      let student = new Student("Tom", 21, 2);
+      // let student = new Student("Tom", 21, 2);
       expect(student.name).toBe("Tom");
       expect(student.age).toBe(21);
       expect(student.klass).toBe(2);
@@ -28,7 +38,7 @@ describe("Person", () => {
     });
 
     it("should overwrite Person introduce, introduce student with class", () => {
-      let student = new Student("Tom", 21, 2);
+      // let student = new Student("Tom", 21, 2);
       let introduce = student.introduce();
 
       expect(introduce).toBe("My name is Tom. I am 21 years old. I am a Student. I am at Class 2.");
